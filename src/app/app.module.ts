6c28b13bd6e05a31e9e7f451ac53fc326be076fb
FrancoisHaegel <haegelfrancois@yahoo.fr>
2019-10-13 20:58:27 +0200

    working on stackblitz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,28 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { TetiereComponent } from './tetiere/tetiere.component';
+import { UserFormComponent } from './user-form/user-form.component';
+import { PhoneNumberPipe } from "./user-form/phoneNumber.pipe";
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    TetiereComponent,
+    UserFormComponent,
+    PhoneNumberPipe,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [
+    PhoneNumberPipe
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
